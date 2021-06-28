b13b632dda5fb47e7bd3b3010b69f50649f15762
Tananiko <tanczosaniko@yahoo.com>
2021-06-28 19:34:58 +0200

    003  hw - bmodul.py and main_bmodul.py done

---
+++ python-homework/bmodul.py
@@ -0,0 +1,8 @@
+var = 1
+
+def fn (a, b):
+    return (a * b)
+def fm (c, d):
+    return (c % d)
+def fo (e, f):
+    return (e // f)
---
+++ python-homework/main_bmodul.py
@@ -0,0 +1,5 @@
+import bmodul
+
+print(bmodul.fn(2, 4))
+print(bmodul.fm(15, 3))
+print(bmodul.fo(12, 8))
